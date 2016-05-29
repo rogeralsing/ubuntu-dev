b7718ec9d046ed61c95fb27afa1c325b8a07f0cf
Roger Johansson <rogeralsing@gmail.com>
2016-05-29 22:11:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,3 +49,4 @@ echo ". /usr/share/autojump/autojump.sh" >> ~/.zshrc
 sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
 echo "export PATH=\"$PATH:/home/rogeralsing/proto:/home/rogeralsing/bin\""  >> ~/.zshrc
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
+gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
